a5b6abb0feb48e309788f4981fde6cfcf1f360e7
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2022-02-05 11:06:52 -0300

    updating variables; minor refactor; added ecr examples; documentation

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Opinionated module for quick Subspace Wireguard VPN deployment on AWS
 ## Introduction
 
 Infrastructure deployment for [subspacecommunity/subspace](https://github.com/subspacecommunity/subspace)
+
+## Documentation
+
+This project is intended to be a thin layer over the subspace project. The added capabilities are:
+- periodic backup to an S3 bucket
+- ssh key written to a bucket of choice, so you can later access the EC2 instance via SSH
+- Web App, Wireguard endpoint and Internal Alias Route53 Records
+
+### Arguments
+
+Refer to subspace documentation in the aforementioned repository. The module variables map directly to
+the subspace configuration variables. The module variables also have descriptions.
--- ec2.tf
+++ ec2.tf
@@ -57,7 +57,7 @@ resource "aws_launch_template" "instance" {
   user_data = base64encode(templatefile(
     "${path.module}/templates/user-data.sh.tpl",
     {
-      aws_region                      = var.region
+      aws_region                      = var.aws_region
       aws_account_id                  = data.aws_caller_identity.current.account_id
       eip_id                          = aws_eip.this.id
       is_ecr_docker_image             = var.is_ecr_docker_image
@@ -66,6 +66,8 @@ resource "aws_launch_template" "instance" {
       wireguard_ipv4_gateway          = var.ipv4_gateway
       wireguard_ipv6_gateway          = var.ipv6_gateway
       wireguard_disable_dns           = var.disable_wireguard_dns
+      subspace_ipv4_nat_enabled       = var.enable_ipv4_nat
+      subspace_ipv6_nat_enabled       = var.enable_ipv6_nat
       wireguard_subspace_http_host    = aws_route53_record.web.fqdn
       wireguard_endpoint_host         = aws_route53_record.endpoint.fqdn
       wireguard_backup_bucket_name    = aws_s3_bucket.backup.bucket
@@ -76,9 +78,11 @@ resource "aws_launch_template" "instance" {
 
 resource "aws_autoscaling_group" "this" {
   name = var.name
-
-  desired_capacity    = var.desired_instances
-  max_size            = var.desired_instances + 1
+  # TODO: implement HA
+  #  desired_capacity    = var.desired_instances
+  #  max_size            = var.desired_instances + 1
+  desired_capacity    = 1
+  max_size            = 1
   min_size            = 0
   vpc_zone_identifier = var.subnet_ids
 
